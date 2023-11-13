7eff8a4ac531fe90eb468209cb724c8938b701ee
isabellaobrien <bellaobrien.it@gmail.com>
2023-11-13 17:00:59 +0000

    Add Sidebar component

---
+++ my-app/src/components/Sidebar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Sidebar() {
+  return (
+    <div>
+        <a href="#">MyPhotos</a>
+        <a href="#">MyIllustrations</a>
+        <a href="#">MyPaintings</a>
+    </div>
+  )
+}
+
+export default Sidebar
