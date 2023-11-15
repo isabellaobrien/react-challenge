db962d80ca04034db2f2fe4d366afee1384bee95
isabellaobrien <bellaobrien.it@gmail.com>
2023-11-15 14:42:43 +0000

    component life cycles

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -2,14 +2,24 @@ import React, { Component } from 'react'
 import css from "./css/Content.module.css"
 import {savedPosts} from "../posts.json"
 import PostItem from './PostItem'
+import Loader from './Loader'
 
 export class Content extends Component {
     constructor(props) {
       super(props)
     
       this.state = {
+        isLoaded: false
       }
     }
+    componentDidMount() {
+        setTimeout(()=>{
+            this.setState({
+                isLoaded: true,
+            })
+        }, 2000)
+    }
+
     render() {
         return (
             <div className={css.Content}>
@@ -17,17 +27,20 @@ export class Content extends Component {
                     <h1>My Photos</h1>
                 </div>
                 <div className={css.SearchResults}>
-                    {/* {
-                        savedPosts.map((post)=>{
-                            return <div className={css.SearchItem} key={title}>
-                                <p>{post.title}</p>
-                                <p>{post.name}</p>
-                                <img src={post.image} alt="random" />
-                                <p>{post.description}</p>
-                            </div>
-                        }) 
-                    } */}
-                    <PostItem savedPosts={savedPosts} />
+                    {
+                        // savedPosts.map((post)=>{
+                        //     return <div className={css.SearchItem} key={title}>
+                        //         <p>{post.title}</p>
+                        //         <p>{post.name}</p>
+                        //         <img src={post.image} alt="random" />
+                        //         <p>{post.description}</p>
+                        //     </div>
+                        // }) 
+                        this.state.isLoaded ?
+                        <PostItem savedPosts={savedPosts} />
+                        : <Loader />
+                    }
+                    {/* <PostItem savedPosts={savedPosts} /> */}
                 </div>
             </div>
         )
